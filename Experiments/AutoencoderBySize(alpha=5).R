8ad4be10743f4c1a45417704c371296f8e64b57e
Robin <robin.vandaele@ugent.be>
2021-12-13 11:52:08 +0100

    visualization AE by size alpha=5

---
+++ Experiments/AutoencoderBySize(alpha=5).R
@@ -0,0 +1,20 @@
+setwd("..")
+
+### Load libraries ###
+
+library("ggplot2") # general plotting
+
+### We study how high-dimensional noise affects the autoencoder dimensionality reduction ###
+
+cors_auto <- read.table("Results/Size/AUTO_alpha5.csv", # load the results
+                        sep=",", row.names=1, header=TRUE)
+ggplot(data=cors_auto, aes(x=dim, y=cor, color=factor(size))) +
+  geom_line() +
+  geom_point() +
+  scale_x_log10() +
+  ylab("correlation") +
+  labs(col="data size") +
+  ggtitle("AutoEncoder") +
+  theme_classic() +
+  theme(text=element_text(size=15), plot.title=element_text(hjust=0.5, size=15),
+        legend.title=element_text(size=20), legend.text=element_text(size=20))
